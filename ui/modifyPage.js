b6b36fe78bc39a7b977903ddeca82b57df455856
Heon Ki <addamea@gmail.com>
2015-04-16 14:26:12 +0900

    1

--- ui/modifyPage.js
+++ ui/modifyPage.js
@@ -45,12 +45,12 @@ function modifyHref()
   alert("test1");
   if (sc.length == 0)
     return;
-  alert("test2");
+  alert("test2"+sc.length);
   
   for (var i=0; i < sc.length; ++i) {
     var as =sc.getElementsByTagName("a");
     // for(var k=0; k < as.length; ++k){
-      var a = as[1];
+      var a = as[i];
       alert(a.href);
       // if(a.href.indexOf("tel:") != 0){
         a.setAttribute("oldHref", a.href);
