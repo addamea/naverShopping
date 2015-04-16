76049ec111e06b003a740c80346c5ef681e54199
Heon Ki <addamea@gmail.com>
2015-04-16 13:57:26 +0900

    1

--- ui/modifyHref.js
+++ ui/modifyHref.js
@@ -1,16 +1,20 @@
 function modifyHref()
 {
-	var sc = document.getElementsByClassName("zzim _sr_zzim");
+	var sc = document.getElementsByClassName("list_btn");
 	if (sc.length == 0)
 		return;
-
-	for(var k=0; k < sc.length; ++k){
-		var a = as[k];
-		a.setAttribute("oldHref", a.href);
-		var script = "device.toastPopup('aaa')";
-		a.setAttribute("href", "javascript:'';");
-		a.setAttribute("onclick", "agate.runAction(\""+script+"\"),undefined");
-	}
+	
+	for (var i=0; i < lis.length; ++i) {
+		var li = lis[i];
+		var as =li.getElementsByTagName("a");
+		// for(var k=0; k < as.length; ++k){
+			var a = as[1];
+			// if(a.href.indexOf("tel:") != 0){
+				a.setAttribute("oldHref", a.href);
+				var script = "device.toastPopup('sss')";
+				a.setAttribute("href", "javascript:agate.runAction(\""+script+"\"),undefined");
+			// }
+		// }
 	}
 	return;
 }
